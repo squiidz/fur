2215719c2f33c9bdd1ff968d38e138a3150390b8
squiidz <piinkyz05@gmail.com>
2014-11-14 16:23:06 -0500

    Modified the context.go file, now using a struct

--- context.go
+++ context.go
@@ -5,39 +5,53 @@ import (
 	"sync"
 )
 
+// Context struct, req = the *http.Request provided with the NewContext func
+type Context struct {
+	req  *http.Request
+	vars map[interface{}]interface{}
+}
+
 var (
-	mut     sync.RWMutex
-	context = make(map[*http.Request]map[interface{}]interface{})
+	mutex sync.RWMutex
+	memo  = make(map[*http.Request]Context)
 )
 
-func Set(req *http.Request, key, value interface{}) {
-	mut.Lock()
-	if context[req] != nil {
-		context[req][key] = value
-	} else {
-		context[req] = make(map[interface{}]interface{})
-		context[req][key] = value
-	}
-	mut.Unlock()
+// Create a new context struct, with vars map
+func NewContext(req *http.Request) *Context {
+	memo[req] = Context{req, make(map[interface{}]interface{})}
+	c := memo[req]
+	return &c
 }
 
-func Get(req *http.Request, key interface{}) interface{} {
-	mut.RLock()
-	if context[req] != nil {
-		return context[req][key]
-	}
-	mut.RUnlock()
-	return nil
+// Retrive a context from the memo map and return.
+func FindContext(req *http.Request) *Context {
+	con := memo[req]
+	return &con
 }
 
-func Delete(req *http.Request, key interface{}) {
-	mut.Lock()
-	if context[req] != nil {
-		delete(context[req], key)
-	}
-	mut.Unlock()
+// Set a new key value pair in the context.Vars
+func (c *Context) Set(key, value interface{}) {
+	mutex.Lock()
+	c.vars[key] = value
+	mutex.Unlock()
 }
 
-func Destroy(req *http.Request) {
-	delete(context, req)
+// Get a key value from a context
+func (c *Context) Get(key interface{}) interface{} {
+	return c.vars[key]
+}
+
+// Get the map of data of the context
+func (c *Context) GetAll() map[interface{}]interface{} {
+	return c.vars
+}
+
+// Delete a key
+func (c *Context) Delete(key interface{}) {
+	delete(c.vars, key)
+}
+
+// Remove a Context from the memo map
+func (c *Context) Destroy() {
+	delete(memo, c.req)
 }
